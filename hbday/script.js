c5e31cc85dace23dd589d1b2ead9c544420af7ef
Rayya R. <106069353+feeldreams@users.noreply.github.com>
2024-08-31 01:07:37 +0700

    Create script.js

---
+++ hbday/script.js
@@ -0,0 +1,182 @@
+const body = document.querySelector("body"); const iniwp = [];iden = 1; const swals = Swal.mixin({timer: 99999, allowOutsideClick: false, showConfirmButton: true, timerProgressBar: false, imageHeight: 90,}); audio = new Audio('' + linkmp3.src); ftganti=0;fungsi=0;fungsiAwal=0;deffotostiker=fotostiker.src;
+function berjatuhan() {const heart = document.createElement("div"); heart.className = "fas fa-heart"; heart.style.left = (Math.random() * 90)+"vw"; heart.style.animationDuration = (Math.random()*3)+2+"s"; body.appendChild(heart);} setInterval(function name(params) {var heartArr = document.querySelectorAll(".fa-heart"); if (heartArr.length > 100) {heartArr[0].remove()}},100);Content.style = "opacity:1;margin-top:14vh"; 
+
+const box = document.getElementById('pergeseran');
+const totalSlide = box.children.length;
+console.log('Skrip dibuat oleh feeldream.id 😋');
+console.log('Total Slide: ', totalSlide);
+totalPesan = totalSlide;
+
+//////////////////////////////////////////////////
+
+  var sudahklik = true;loveIn.innerHTML = "<label class='lovein'><svg class='line' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><g transform='translate(2.550170, 3.550158)'><path d='M0.371729633,8.89614246 C-0.701270367,5.54614246 0.553729633,1.38114246 4.07072963,0.249142462 C5.92072963,-0.347857538 8.20372963,0.150142462 9.50072963,1.93914246 C10.7237296,0.0841424625 13.0727296,-0.343857538 14.9207296,0.249142462 C18.4367296,1.38114246 19.6987296,5.54614246 18.6267296,8.89614246 C16.9567296,14.2061425 11.1297296,16.9721425 9.50072963,16.9721425 C7.87272963,16.9721425 2.09772963,14.2681425 0.371729633,8.89614246 Z'></path><path d='M13.23843,4.013842 C14.44543,4.137842 15.20043,5.094842 15.15543,6.435842'></path></g></svg></label>";
+  document.getElementById("loveIn").onclick = function() {
+      if(sudahklik == true && fungsiAwal==0){
+        loveIn.style="transition:all .5s ease;opacity:0";
+        ftAwal.style="transition:all .5s ease;opacity:0";
+        ket.style="transition:all .5s ease;opacity:0";
+        link.style="transition:all .5s ease;opacity:0";
+        fungsiAwal=1;setTimeout(initengahan,300);
+      } else {
+      	sudahklik = true;
+      	loveIn.innerHTML = "<label class='lovein'><svg class='line' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><g transform='translate(2.550170, 3.550158)'><path d='M0.371729633,8.89614246 C-0.701270367,5.54614246 0.553729633,1.38114246 4.07072963,0.249142462 C5.92072963,-0.347857538 8.20372963,0.150142462 9.50072963,1.93914246 C10.7237296,0.0841424625 13.0727296,-0.343857538 14.9207296,0.249142462 C18.4367296,1.38114246 19.6987296,5.54614246 18.6267296,8.89614246 C16.9567296,14.2061425 11.1297296,16.9721425 9.50072963,16.9721425 C7.87272963,16.9721425 2.09772963,14.2681425 0.371729633,8.89614246 Z'></path><path d='M13.23843,4.013842 C14.44543,4.137842 15.20043,5.094842 15.15543,6.435842'></path></g></svg></label>";
+      }
+    }
+  
+  function initengahan(){
+    link.style="display:none";ftAwal.style="display:none";loveIn.style="display:none";ket.style="display:none";
+    Content.style = "opacity:1;margin-top:10vh";
+    setTimeout(inipesan,200);audio.play();
+  }
+  
+aktigeser=0;thisgeser=1;
+document.getElementById("bodyblur").onclick = function() {multifungsi()}
+
+function multifungsi(){
+  if(aktigeser==1){
+    if(thisgeser==totalPesan){aksiakhir()}
+    document.getElementById('pergeseran').scrollLeft += 300;
+    hsementara();
+    keterangan.style="";
+    //if(thisgeser<=totalPesan){setTimeout(function(){keterangan.style="opacity:.7";},500);}
+    //ftganti++;
+    //fthilang();
+    if(thisgeser>totalPesan){document.getElementById('dots-container').style="";} else {moveToNextDot()}
+    //if(thisgeser==3){balon1.style="position:absolute;top:20px;left:40px;opacity:.8;font-size:25px";balon2.style="position:absolute;bottom:20px;right:35px;opacity:.8;font-size:25px";} else {balon1.style="opacity:0";balon2.style="opacity:0";}
+  }
+  if(thisgeser==100){Tombol.style="";setTimeout(aksibalas,150);}
+}
+
+  async function inipesan(){nama = "Kamu";window.nama = nama;mulainama();}  
+  
+  async function mulainama() {
+    setTimeout(pgmuncul,200);
+    //ftmuncul();
+  }
+
+  function hsementara(){
+    //ketgeser.style="position:relative;";
+    Tombol.style="";
+    thisgeser+=1;aktigeser=0;setTimeout(munculkembali,500)
+  }
+  function munculkembali(){
+    if(thisgeser<=totalPesan){
+      //ketgeser.style="position:relative;transform:scale(1);opacity:.7";
+      Tombol.style="opacity:1;transform: scale(1)";
+      aktigeser=1;
+    }
+    if(thisgeser==50){tmbl.innerHTML = "💌 Balas";Tombol.style="opacity:1;transform: scale(1)";thisgeser=100;}
+    //if(thisgeser==totalPesan){aksiakhir();thisgeser=50}
+  }
+  
+  function aksiakhir(){
+  	   pergeseran.style="position:relative;";
+         //ketgeser.style="position:relative";
+         Tombol.style="";
+         //setTimeout(aksibalas,600);
+         poinjwb=1;
+         bodyblur.style="opacity:.2";
+         setTimeout(function(){document.getElementById("gift").classList.add("animate");document.getElementById("teksgift").classList.add("textanimate");setTimeout(bqmuncul,2000);},600);
+  }
+  
+  function kalimatakhir(){
+  	new TypeIt("#kalimat", {
+      strings: ["" + katakata], startDelay: 50, speed: 37, cursor: true,
+      afterComplete: function(){
+      	kalimat.innerHTML = katakata;
+          setTimeout(munculteksnim,300);
+          //tmbl.innerHTML = "💌 Balas";thisgeser=100;
+          //Tombol.style="opacity:1;transform: scale(1)";
+      },}).go();
+  }
+  function munculteksnim(){
+    //klganti.style="display:none";
+    teksnim.style="position:relative;opacity:1;transform:scale(1);margin:20px auto";
+    setTimeout(jjteksnim,550);
+    ftganti=1;fthilang();
+    //setInterval(berjatuhan,250);
+  }
+  function jjteksnim(){teksnim.style.animation="rto .8s infinite alternate";}
+  
+  function ftmuncul(){
+    if(ftganti==0){fotostiker.src = deffotostiker;}
+    if(ftganti==1){fotostiker.src = fotostiker1.src;}
+    if(ftganti==2){fotostiker.src = fotostiker2.src;}
+    if(ftganti==3){fotostiker.src = fotostiker3.src;}
+    if(ftganti<=10){fotostiker.style="display:inline-flex;opacity:1;transform:scale(1)";}
+  }
+  function fthilang(){fotostiker.style="display:inline-flex;opacity:0;transform:scale(0)";if(ftganti<10){setTimeout(ftmuncul,250)}}
+  function jjfoto(){fotostiker.style.animation="rto .8s infinite alternate";}
+  
+  function pgmuncul(){pergeseran.style="position:relative;opacity:1;transform:scale(1);";keterangan.style="opacity:.7";document.getElementById('dots-container').style="opacity:1";setTimeout(munculkembali,500)}
+  function bqmuncul(){
+    bodyblur.style="";
+    if(poinjwb==1){
+      katakata = kalimat.innerHTML;kalimat.innerHTML = "";
+    }else{
+      klganti.innerHTML="Udah ah segitu aja 🤣<br><br>";katakata = kalimat.innerHTML;kalimat.innerHTML = "";
+    }
+    Content.style = "opacity:1;margin-top:7vh";fotostiker.style="display:none";pergeseran.style="display:none";Tombol.style="";bq.style = "position:relative;opacity:1;visibility:visible;margin-top:0px;transform: scale(1);";
+    setTimeout(kalimatakhir,200);ftganti=0;fthilang();
+  }
+  function bqhilang(){wallpaper.style="transform: scale(2);";bodyblur.style="opacity:.3";bq.style = "position:relative;transition:all .7s ease;";}
+  
+  tompositif = "Mau";
+  tomnegatif = "Gamau";
+  async function aksibalas(){
+    var { isConfirmed: prtanya } = await swals.fire({
+      title: 'Mau Kado Gak Nih? 🤭❤️',
+      imageUrl: '' + fotostikerPopup.src, showCancelButton: true, confirmButtonText: '' + tompositif, cancelButtonText: '' + tomnegatif, cancelButtonColor: '#FF0000',});
+    if(prtanya){
+       await swals.fire({
+         title: 'Tapi Boong! 🤣', 
+         html: 'Gajadi ngasih kado ah 😜❤️', 
+         imageUrl: '' + fotostikerPopupCon.src,
+       });
+       poinjwb=1;
+    } else {
+	     await swals.fire({
+         title: 'Yaaahh!', 
+         html: 'Yaudah kalo gamau 😜❤️', 
+         imageUrl: '' + fotostikerPopupCan.src,
+       });
+       poinjwb=2;
+    }
+    bqmuncul();
+    }
+    
+    const pergeseran = document.getElementById('pergeseran');
+    const dotsContainer = document.getElementById('dots-container');
+ 
+    // Ambil semua elemen <p> di dalam <div id='pergeseran'>
+    const paragraphs = pergeseran.getElementsByTagName('p');
+    const numberOfDots = paragraphs.length;
+
+    // Buat dot sesuai dengan jumlah <p> dan tambahkan ke dalam dotsContainer
+    for (let i = 0; i < numberOfDots; i++) {
+        const dot = document.createElement('div');
+        dot.className = 'dot';
+        
+        // Jika dot pertama, tambahkan kelas 'active'
+        if (i === 0) {
+            dot.classList.add('active');
+        }
+
+        // Tambahkan dot ke dalam dotsContainer
+        dotsContainer.appendChild(dot);
+    }
+
+    // Fungsi untuk mengubah dot aktif ke dot berikutnya
+    function moveToNextDot() {
+        const activeDot = document.querySelector('.dot.active');
+        let nextDot = activeDot.nextElementSibling;
+
+        // Jika tidak ada dot berikutnya, kembali ke dot pertama
+        if (!nextDot) {
+            nextDot = dotsContainer.firstElementChild;
+        }
+
+        // Ubah kelas active ke dot berikutnya
+        activeDot.classList.remove('active');
+        nextDot.classList.add('active');
+    }
