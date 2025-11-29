7e62c6c3069c4b2b1b73cd276edffe99304cf016
Rayya R. <106069353+feeldreams@users.noreply.github.com>
2025-11-29 10:39:48 +0700

    Update script.js

--- teruntukguru/prod/script.js
+++ teruntukguru/prod/script.js
@@ -232,6 +232,6 @@ audio = new Audio('' + linkmp3.src);
     // Share ke WhatsApp
     function balasWa() {
       const url = window.location.href;
-      const text = ";
+      const text = "Test";
       window.open(`https://wa.me/?text=${encodeURIComponent(text)}`, '_blank');
     }
